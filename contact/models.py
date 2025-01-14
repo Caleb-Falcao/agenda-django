f5b0fb438e9e89f8889a685ea6137c4ad79a5132
Caleb Falcão <calebfalcao1@gmail.com>
2025-01-14 19:16:13 -0300

    Criação das models e urls do sistema

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,51 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Category (models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:  # Define o método especial __str__ que retorna uma string.
+        return self.name  # Retorna uma string formatada com o primeiro nome
+
+class Contact(models.Model):
+    first_name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    last_name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    phone = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    email = models.EmailField(
+        max_length=254,
+        blank=True
+    )
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True, 
+        null=True)
+
+    def __str__(self) -> str:  # Define o método especial __str__ que retorna uma string.
+        return f'{self.first_name} {self.last_name}'  # Retorna uma string formatada com o primeiro nome e o sobrenome separados por um espaço.
+
+    
